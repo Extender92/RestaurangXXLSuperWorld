7086c940e8b85cae40a3b31023063b2b587f9c2a
Bertwald <thomas_ace8@hotmail.com>
2022-10-31 07:54:12 +0100

    Structure and Person Base class

--- RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Persons/NameGenerator.cs
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Persons/NameGenerator.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RestaurangXXLSuperWorld {
+namespace RestaurangXXLSuperWorld.Persons
+{
     /**
      * A class for creating randomly generated swedish full names
      * 
      */
-    internal sealed class NameGenerator {
+    internal sealed class NameGenerator
+    {
         private static readonly Random random = new();
         private static readonly string[] names1 = {
                 "Karin",
@@ -51,11 +53,12 @@ namespace RestaurangXXLSuperWorld {
         /**
          * A method that returns a full (Swedish) name 
          */
-        internal static string GetName() {
+        internal static string GetName()
+        {
             return names1[random.Next(names1.Length)] +
              " " + names2[random.Next(names2.Length)];
         }
         //Good luck ever obtaining an instance of this class 
-        private NameGenerator() { ; }
+        private NameGenerator() {; }
     }
 }
---
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Persons/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurangXXLSuperWorld.Persons {
+    internal abstract class Person {
+        private readonly string _name;
+        public string Name { get => _name; }
+        public Person() {
+            _name = NameGenerator.GetName();
+        }
+    }
+}
