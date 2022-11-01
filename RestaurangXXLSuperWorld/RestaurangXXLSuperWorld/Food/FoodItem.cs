e8c6326821e0ccacf9bf24cfcd29b7fe7cb816ef
Bertwald <thomas_ace8@hotmail.com>
2022-11-01 14:07:40 +0100

    Additions to FoodItem base class

--- RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Food/FoodItem.cs
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Food/FoodItem.cs
@@ -8,11 +8,32 @@ namespace RestaurangXXLSuperWorld.Food
 {
     internal class FoodItem
     {
-        public double QualityLevel { get; init; }
+        protected int _currentStep;
+        protected DateTime? _timeDone = null;
+        public double QualityLevel { get; set; }
         public string Name { get; init; }
+        public bool IsDone { get; private set; }
+        public DateTime? TimeDone 
+        { 
+            get { if (IsDone && _timeDone != null) { return _timeDone; } return null;} 
+            set { _timeDone = value; } 
+        }
         public int Cost { get; init; }
         public int TimeToCook { get; init; }
 
-        internal FoodItem(string name) { Name = name; }
+        internal FoodItem(string name) 
+        {
+            _currentStep = 0;
+            Name = name;
+            TimeToCook = 10;
+        }
+        internal void Cook() {
+            _currentStep++;
+            if(_currentStep == 10) {
+                IsDone = true;
+                TimeDone = DateTime.Now;
+            }
+        }
+        internal virtual string GetCurrentStep() { return "Cooking the idea of Food"; }
     }
 }
