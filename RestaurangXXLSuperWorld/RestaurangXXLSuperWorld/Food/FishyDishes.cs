49a5a315cae78996ad2f7905839251557379bb32
oscar3L <oscar@longi.se>
2022-11-01 12:06:48 +0100

    FishyDicks
    Food klasser

---
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Food/FishyDishes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurangXXLSuperWorld.Food
+{
+    internal class FishyDishes : FoodItem
+    {
+        internal FishyDishes (string Name) : base (Name) { }
+    }
+    internal class FishOne : FishyDishes
+    {
+        public FishOne() : base("Spaghetti with pesto, crushed potato and salmon") { }
+    }
+    internal class FishTwo : FishyDishes
+    {
+        public FishTwo() : base("Crispy Homemade Fish Dicks with white sauce") { }
+    }
+    internal class FishThree : FishyDishes
+    {
+        public FishThree() : base("California Sushi Rolls") { }
+    }
+}
---
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Food/MeatDishes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurangXXLSuperWorld.Food
+{
+    internal class MeatDishes : FoodItem
+    {
+        internal MeatDishes (string Name) : base(Name) { }
+    }
+
+    internal class MeatOne : MeatDishes
+    {
+        public MeatOne () : base("Crackling pork roast with pickled onions") { }
+    }
+    internal class MeatTwo : MeatDishes
+    {
+        public MeatTwo() : base("Roast beef with red wine & banana shallots") { }
+    }
+    internal class MeatThree : MeatDishes
+    {
+        public MeatThree() : base("T-bone steak with fries and béarnaise sauce") { }
+    }
+}
