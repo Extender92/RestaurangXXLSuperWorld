da2304dd6a86d415bd5a65e3f04bb5c799fb919b
Extender <dann_137@hotmail.com>
2022-11-01 14:21:45 +0100

    GUI
    New GUI Class

--- RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Program.cs
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Program.cs
@@ -1,29 +1,16 @@
-﻿namespace RestaurangXXLSuperWorld
+﻿using RestaurangXXLSuperWorld.View;
+
+namespace RestaurangXXLSuperWorld
 {
     internal class Program
     {
         static void Main()
         {
-            //char[,] characters = { { '╚', '╝', '╬', '═', '╩', '╠', '╣', '╦', '╔', '╗', '║' }, { '└', '┘', '┼', '─', '┴', '├', '┤', '┬', '┌', '┐', '│' } };
-            //                          0    1    2    3    4    5    6    7    8    9    10       0    1    2    3    4    5    6    7    8    9    10
-
-            //char[][] characters = { "╚╝╬═╩╠╣╦╔╗║".ToCharArray(), "└┘┼─┴├┤┬┌┐│".ToCharArray() };
-
-            //int[] intsOne = { 8, 3, 9, 10, 0, 1 };
-            //int[] intsTwo = { 7, 3, 7, 10, 0, 1 };
-            //int[][] charSet = { intsOne, intsTwo };
-
-            char[] restaurangChar = { '╔', '═', '╗', '║', '╚', '╝' };
-            char[] kitchenChar = { '╦', '═', '╦', '║', '╚', '╝' };
-            char[] tableChar = { '┌', '─', '┐', '│', '└', '┘' };
 
 
+            
 
-            Printer(110, 20, 1, 1, restaurangChar, ConsoleColor.Red);
-            Printer(5, 2, 3, 2, tableChar, ConsoleColor.Blue);
-
-            Printer(10, 4, 50, 1, kitchenChar, ConsoleColor.Red);
-
+            GUI.DrawRestaurant();
 
 
             Console.ReadLine();
@@ -31,38 +18,6 @@
         }
 
 
-        private static void Printer(int width, int height, int positionX, int positionY, char[] characters, ConsoleColor color)
-        {
-            int[] cursorPosition = { positionX, positionY };
-            Console.ForegroundColor = color;
-
-            Console.SetCursorPosition(cursorPosition[0], cursorPosition[1]);
-            Console.Write(characters[0]);
-            for (int i = 0; i < width; i++)
-            {
-                Console.Write(characters[1]);
-            }
-            Console.Write(characters[2]);
-
-            for (int i = 0; i < height; i++)
-            {
-                cursorPosition[1] = i + positionY + 1;
-                Console.SetCursorPosition(cursorPosition[0], cursorPosition[1]);
-                Console.Write(characters[3]);
-                cursorPosition[0] = width + positionX + 1;
-                Console.SetCursorPosition(cursorPosition[0], cursorPosition[1]);
-                Console.Write(characters[3]);
-                cursorPosition[0] = positionX;
-            }
-
-            cursorPosition[1]++;
-            Console.SetCursorPosition(cursorPosition[0], cursorPosition[1]);
-            Console.Write(characters[4]);
-            for (int i = 0; i < width; i++)
-            {
-                Console.Write(characters[1]);
-            }
-            Console.Write(characters[5]);
-        }
+     
     }
 }
---
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/View/GUI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurangXXLSuperWorld.View
+{
+    internal class GUI
+    {
+        internal static void DrawRestaurant()
+        {
+            //char[,] characters = { { '╚', '╝', '╬', '═', '╩', '╠', '╣', '╦', '╔', '╗', '║' }, { '└', '┘', '┼', '─', '┴', '├', '┤', '┬', '┌', '┐', '│' } };
+            //                          0    1    2    3    4    5    6    7    8    9    10       0    1    2    3    4    5    6    7    8    9    10
+
+            //char[][] characters = { "╚╝╬═╩╠╣╦╔╗║".ToCharArray(), "└┘┼─┴├┤┬┌┐│".ToCharArray() };
+
+            //int[] intsOne = { 8, 3, 9, 10, 0, 1 };
+            //int[] intsTwo = { 7, 3, 7, 10, 0, 1 };
+            //int[][] charSet = { intsOne, intsTwo };
+
+            char[] restaurangChar = { '╔', '═', '╗', '║', '╚', '╝' };
+            char[] kitchenChar = { '╦', '═', '╦', '║', '╚', '╝' };
+            char[] tableChar = { '┌', '─', '┐', '│', '└', '┘' };
+
+            RestaurantPrinter(110, 20, 1, 1, restaurangChar, ConsoleColor.Red);
+            RestaurantPrinter(10, 4, 50, 1, kitchenChar, ConsoleColor.Red);
+
+            RestaurantPrinter(5, 2, 3, 2, tableChar, ConsoleColor.Blue);
+        }
+
+        private static void RestaurantPrinter(int width, int height, int positionX, int positionY, char[] characters, ConsoleColor color)
+        {
+            int[] cursorPosition = { positionX, positionY };
+            Console.ForegroundColor = color;
+
+            Console.SetCursorPosition(cursorPosition[0], cursorPosition[1]);
+            Console.Write(characters[0]);
+            for (int i = 0; i < width; i++)
+                Console.Write(characters[1]);
+            Console.Write(characters[2]);
+
+            for (int i = 0; i < height; i++)
+            {
+                cursorPosition[1] = i + positionY + 1;
+                Console.SetCursorPosition(cursorPosition[0], cursorPosition[1]);
+                Console.Write(characters[3]);
+                cursorPosition[0] = width + positionX + 1;
+                Console.SetCursorPosition(cursorPosition[0], cursorPosition[1]);
+                Console.Write(characters[3]);
+                cursorPosition[0] = positionX;
+            }
+
+            cursorPosition[1]++;
+            Console.SetCursorPosition(cursorPosition[0], cursorPosition[1]);
+            Console.Write(characters[4]);
+            for (int i = 0; i < width; i++)
+                Console.Write(characters[1]);
+            Console.Write(characters[5]);
+        }
+    }
+}
