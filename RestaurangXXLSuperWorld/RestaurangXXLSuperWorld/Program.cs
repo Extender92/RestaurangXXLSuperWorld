41dc3f91dec5a3ebd9c39df48d3fdbed239816d2
oscar3L <oscar@longi.se>
2022-11-02 14:37:00 +0100

    gui table update

--- RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Program.cs
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Program.cs
@@ -7,35 +7,35 @@ namespace RestaurangXXLSuperWorld
     {
         static void Main()
         {
-            //PersonTester
-            Person p1 = new Customer();
-            Person p2 = new Customer();
-            Console.WriteLine(p1.Name);
-            Console.WriteLine(p2.Name);
+        //    //PersonTester
+        //    Person p1 = new Customer();
+        //    Person p2 = new Customer();
+        //    Console.WriteLine(p1.Name);
+        //    Console.WriteLine(p2.Name);
 
-            //Party Tester
-            Party<Customer> party1 = new();
-            Party<Customer> party2 = new();
-            Console.WriteLine(party1.Size());
-            Console.WriteLine(party2.Size());
+        //    //Party Tester
+        //    Party<Customer> party1 = new();
+        //    Party<Customer> party2 = new();
+        //    Console.WriteLine(party1.Size());
+        //    Console.WriteLine(party2.Size());
 
-            //QueueTester
-            RestaurantQueue<Party<Customer>> queue = RestaurantQueue<Party<Customer>>.InitializeQueue(80);
-            party1 = queue.GetFirstInQueue();
-            party2 = queue.GetSuitableParty(1);
-            Console.WriteLine(party1.Size());
-            Console.WriteLine(party2.Size());
+        //    //QueueTester
+        //    RestaurantQueue<Party<Customer>> queue = RestaurantQueue<Party<Customer>>.InitializeQueue(80);
+        //    party1 = queue.GetFirstInQueue();
+        //    party2 = queue.GetSuitableParty(1);
+        //    Console.WriteLine(party1.Size());
+        //    Console.WriteLine(party2.Size());
 
-            var persons = party1.getParty();
-            foreach(Customer customer in persons) {
-                Console.WriteLine(customer.Satisfaction);
-                customer.ModifySatisfaction(10d,10d,10d);
-                Console.WriteLine(customer.Name);
-                Console.WriteLine(customer.Satisfaction);
-            }
+        //    var persons = party1.getParty();
+        //    foreach(Customer customer in persons) {
+        //        Console.WriteLine(customer.Satisfaction);
+        //        customer.ModifySatisfaction(10d,10d,10d);
+        //        Console.WriteLine(customer.Name);
+        //        Console.WriteLine(customer.Satisfaction);
+        //    }
 
-            //GUI.DrawRestaurant();
-            //Console.ReadLine();
+            GUI.DrawRestaurant();
+            Console.ReadLine();
 
         }
 
