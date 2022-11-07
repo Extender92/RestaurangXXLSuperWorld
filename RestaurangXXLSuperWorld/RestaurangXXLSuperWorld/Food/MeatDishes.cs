c380041b9f4b31d6a878e3babdec5bc7d39ed474
Bertwald <thomas_ace8@hotmail.com>
2022-11-07 09:50:14 +0100

    "Tillagning" av dishes

--- RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Food/MeatDishes.cs
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Food/MeatDishes.cs
@@ -13,14 +13,56 @@ namespace RestaurangXXLSuperWorld.Food
 
     internal class MeatOne : MeatDish
     {
-        public MeatOne () : base("Crackling pork roast with pickled onions") { }
+        public MeatOne () : base("Crackling pork roast with pickled onions") {
+            Price = 185;
+            _cookingSteps = new string[] {
+                "Förbereder Köksredskap",
+                "HACKA LÖKEN!!!!!!",
+                "Kryddar fläskstek",
+                "Picklar lök",
+                "Tar ut fläskstek ur ugn",
+                "Lagar gräddsås",
+                "Kokar Potatis",
+                "MOSAR POTATIS!!!!!",
+                "Lägger upp på planka",
+                "Lägger på picklad lök"
+            };
+        }
     }
     internal class MeatTwo : MeatDish
     {
-        public MeatTwo() : base("Roast beef with red wine & banana shallots") { }
+        public MeatTwo() : base("Roast beef with red wine & banana shallots") {
+            Price = 210;
+            _cookingSteps = new string[] {
+                "Förbereder Köksredskapen",
+                "HACKA LÖKEN!!!!!!",
+                "Bryner Rostbiff",
+                "Lägger rostbiff i ugn",
+                "Korkar upp vin",
+                "Smakar av vin",
+                "Rostar lök",
+                "Dricker lite mer vin",
+                "Lägger upp rostbiff på trancherbräda",
+                "Häller upp några glas vin"
+            };
+        }
     }
     internal class MeatThree : MeatDish
     {
-        public MeatThree() : base("T-bone steak with fries and béarnaise sauce") { }
+        public MeatThree() : base("T-bone steak with fries and béarnaise sauce") {
+            Price = 250;
+            _cookingSteps = new string[] {
+                "Förbereder Köksredskapen",
+                "HACKA LÖKEN!!!!!!",
+                "Skivar kött",
+                "Grillar kött",
+                "Kokar potatis",
+                "Skär potatis i stavar",
+                "Friterar potatis",
+                "Gör sauce bearnaise",
+                "Lägger upp mat på varm metallbricka",
+                "Lägger på persilja och dragon"
+            };
+        }
     }
 }
--- RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Food/VegetarianDishes.cs
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Food/VegetarianDishes.cs
@@ -12,14 +12,56 @@ namespace RestaurangXXLSuperWorld.Food
     }
     internal class VegOne : VegetarianDish
     {
-        public VegOne() : base("Sweet & sour lentil dhal with grilled aubergine") { }
+        public VegOne() : base("Sweet & sour lentil dhal with grilled aubergine") {
+            Price = 140;
+            _cookingSteps = new string[] {
+                "Förbereder Köksredskapen",
+                "HACKA LÖKEN!!!!!!",
+                "Skivar Aubergine",
+                "Kokar linser",
+                "Kryddar linser",
+                "Grillar aubergine",
+                "Grillar naan",
+                "Vitlökar naan",
+                "Lägger upp dhaal i en skål",
+                "Lägger naan och grillad aubergine på sidofat"
+            };
+        }
     }
     internal class VegTwo : VegetarianDish
     {
-        public VegTwo() : base("Caesar Salad with crisp homemade croutons and a light caesar dressing") { }
+        public VegTwo() : base("Caesar Salad with crisp homemade croutons and a light caesar dressing") {
+            Price = 180;
+            _cookingSteps = new string[] {
+                "Förbereder Köksredskapen",
+                "HACKA LÖKEN!!!!!!",
+                "Lagar krutonger",
+                "Hackar sardeller",
+                "Steker Kyckling",
+                "Steker bacon",
+                "Gör dressing",
+                "Hackar ingredienser och bygger sallad",
+                "Lägger upp sallad, häller över dressing",
+                "River parmesan över sallad och strör på krutonger"
+            };
+        }
     }
     internal class VegThree : VegetarianDish
     {
-        public VegThree() : base("Aloo Gobi with Potatoes & Cauliflower") { }
+        public VegThree() : base("Aloo Gobi with Potatoes & Cauliflower") {
+            Price = 120;
+            _cookingSteps = new string[] {
+                "Förbereder Köksredskapen",
+                "HACKA LÖKEN!!!!!!",
+                "Hackar potatis i små bitar",
+                "Skär blomkål i buketter",
+                "Hackar tomat",
+                "Fräser lök",
+                "Steker lök, blomkål och potatis",
+                "Kryddar och tillsätter tomat",
+                "Lägger upp på ett stort fat",
+                "Beundrar sitt mästerverk"
+            };
+        }
     }
 }
