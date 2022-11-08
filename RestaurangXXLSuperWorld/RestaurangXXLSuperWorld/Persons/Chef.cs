2e25a8c0f65d45746aa3c43f33f258da0ea4ae83
Extender <dann_137@hotmail.com>
2022-11-08 13:02:14 +0100

    BugFix

--- RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Persons/Chef.cs
+++ RestaurangXXLSuperWorld/RestaurangXXLSuperWorld/Persons/Chef.cs
@@ -3,7 +3,6 @@ using RestaurangXXLSuperWorld.RestaurantLogic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
